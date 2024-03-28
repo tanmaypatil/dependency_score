d78a2f46de4e3baa7a1c87eea0fdf2b417368530
Tanmay Patil <tany.patil77@gmail.com>
2024-03-29 00:18:32 +0530

    added a drop down for selecting requirement xlsx

--- ui/ui_graph.py
+++ ui/ui_graph.py
@@ -1,11 +1,25 @@
 import gradio as gr
+from pathlib import Path
+import os
 
-def showimg():
+def getlist():
+    dir_name = Path("requirements")
+    file_list=[]
+    if dir_name.exists():
+       file_list=os.listdir(dir_name) 
+    else:
+        print(f"{os.getcwd()}")
+    return file_list
+
+    
+
+def showimg(file_name):
+    print (f"file selected {file_name}")
     return 'result.svg'
 
 demo = gr.Interface(
     fn=showimg,
-    inputs=None,
+    inputs =[gr.Dropdown(getlist(), label="requirement xlsx", info="select current PI requirement xlsx"),],
     outputs="image",
 )
 if __name__ == "__main__":
